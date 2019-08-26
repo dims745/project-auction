39cd541d7859f065ed9cea598278cb6911878b4c
Dima Tymoshenko <dimatimoshen@gmail.com>
2019-08-27 00:00:29 +0300

    commit 2

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AutorizationController } from './autorization/autorization.controller';
+import { AutorizationService } from './autorization/autorization.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entity/user.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AutorizationController],
+  providers: [AutorizationService],
 })
 export class AuthModule {}
