5dd318eb2a77005af3f6951056d8c7e57e82d706
Dima Tymoshenko <dimatimoshen@gmail.com>
2019-08-27 10:48:44 +0300

    commit login

--- src/auth/autorization/autorization.controller.ts
+++ src/auth/autorization/autorization.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { LoginUserDto } from 'src/DTO/loginUserDto';
 
 @Controller('login')
 export class AutorizationController {
 
     @Get()
-    putLoginForm() {}
+    putLoginForm(): string {
+        const file = readFileSync('1.html' , 'utf8');
+        return file;
+    }
+
+    @Post()
+    checkLogin(@Body() createUserDto: LoginUserDto) {
+        // обработка введенных данных
+        // редирект на главную страничку или вывод формы с сообщением о неудачной попытке
+    }
 }
